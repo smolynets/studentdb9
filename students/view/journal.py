9a3f3c8f581c5495ff12a2ca7b908cbf91bebb68
Oleh Smolynets <smolynets@gmail.com>
2016-10-09 22:06:07 +0300

    page 409 (correct)

--- students/view/journal.py
+++ students/view/journal.py
@@ -12,58 +12,45 @@ class JournalView(TemplateView):
   def get_context_data(self, **kwargs):
     # get context data from TemplateView class
     context = super(JournalView, self).get_context_data(**kwargs)
-    # check if we need to display some specific month
     if self.request.GET.get('month'):
-      month = datetime.strptime(self.request.GET['month'], '%Y-%m-%d').date()
+           month = datetime.strptime(self.request.GET['month'], '%Y-%m-%d').date()
     else:
-      # otherwise just displaying current month data
-      today = datetime.today()
-      month = date(today.year, today.month, 1)
-      # calculate current, previous and next month details;
-      # we need this for month navigation element in template
-      next_month = month + relativedelta(months=1)
-      prev_month = month - relativedelta(months=1)
-      context['prev_month'] = prev_month.strftime('%Y-%m-%d')
-      context['next_month'] = next_month.strftime('%Y-%m-%d')
-      context['year'] = month.year
-      context['month_verbose'] = month.strftime('%B')
-      # we’ll use this variable in students pagination
-      context['cur_month'] = month.strftime('%Y-%m-%d')
-      # prepare variable for template to generate
-      # journal table header elements
-      myear, mmonth = month.year, month.month
-      number_of_days = monthrange(myear, mmonth)[1]
-      context['month_header'] = [{'day': d,
-         'verbose': day_abbr[weekday(myear, mmonth, d)][:2]}
-         for d in range(1, number_of_days+1)]
-      # get all students from database
-      queryset = Student.objects.all().order_by('last_name')
-      # url to update student presence, for form post
-      update_url = reverse('journal')
-      # go over all students and collect data about presence
-      # during selected month
-      students = []
-      for student in queryset:
-      # try to get journal object by month selected
-      # month and current student
-          try:
-             journal = MonthJournal.objects.get(student=student, date=month)
-          except Exception:
-             journal = None
-          # fill in days presence list for current student
-          days = []
-          for day in range(1, number_of_days+1):
-            days.append({
-               'day': day,
-               'present': journal and getattr(journal, 'present_day%d' %day, False) or False,
-               'date': date(myear, mmonth, day).strftime('%Y-%m-%d'),})
-           # prepare metadata for current student
-            students.append({
-             'fullname': u'%s %s' % (student.last_name, student.first_name),'days': days,'id': student.id,
-             'update_url': update_url,})
-           # apply pagination, 10 students per page
-      context = paginate(students, 10, self.request, context, var_name='students')
-     # finally return updated context
-     # with paginated 
-
+           # otherwise just displaying current month data
+           today = datetime.today()
+           month = date(today.year, today.month, 1)
+    next_month = month + relativedelta(months=1)
+    prev_month = month - relativedelta(months=1)
+    context['prev_month'] = prev_month.strftime('%Y-%m-%d')
+    context['next_month'] = next_month.strftime('%Y-%m-%d')
+    context['year'] = month.year
+    context['cur_month'] = month.strftime('%Y-%m-%d')
+    context['month_verbose'] = month.strftime('%B')
+    myear, mmonth = month.year, month.month
+    number_of_days = monthrange(myear, mmonth)[1]
+    context['month_header'] = [{'day': d,'verbose': day_abbr[weekday(myear, mmonth, d)][:2]}
+        for d in range(1, number_of_days+1)]
+    queryset = Student.objects.all().order_by('last_name')
+    update_url = reverse('journal')
+    students = []
+    for student in queryset:
+      try:
+          journal = MonthJournal.objects.get(student=student, date=month)
+      except Exception:
+          journal = None
+      days = []
+      for day in range(1, number_of_days+1):
+        days.append({
+          'day': day,
+          'present': journal and getattr(journal, 'present_day%d' %  day, False) or False,
+          'date': date(myear, mmonth, day).strftime('%Y-%m-%d'),
+          })
+      students.append({
+        'fullname': u'%s %s' % (student.last_name, student.first_name),
+        'days': days,
+        'id': student.id,
+        'update_url': update_url,
+      })
+    context = paginate(students, 2, self.request, context, var_name='students')
     return context
+
+        
